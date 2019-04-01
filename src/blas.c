f2888efd78e2523ba405041c9c65ba780dbbb9f6
Margrit Klitz <margrit.klitz@dlr.de>
2019-04-01 11:00:12 -0700

    Empty framework to solve Blatt1

--- src/blas.c
+++ src/blas.c
@@ -1,73 +1,42 @@
 #include "blas.h"
-
-// method to compute \vec{y} = \alpha \vec{x} + \vec{y}
+/* Remove these comments to implement the function bodies
 void axpy(REAL alpha, REAL* x, REAL* y, int len)
 {
-	for (int i=0;i<len;i++)
-	{
-		y[i]=alpha*x[i]+y[i];
-	}
+
 }
 
 REAL dot(REAL* x, REAL* y, int len)
 {
-	REAL result=0.0;
 
-	for (int i=0;i<len;i++)
-	{
-		result+=x[i]*y[i];
-	}
-
-	return result;
 }
 
 REAL nrm2(REAL* x, int len)
 {
-	return sqrt(dot(x,x,len));
+
 }
 
 void copy(REAL* x, REAL* y, int len)
 {
-	for (int i=0;i<len;i++)
-	{
-		y[i]=x[i];
-	}
+
 }
 
 void scal(REAL alpha, REAL* x, int len)
 {
-	for (int i=0;i<len;i++)
-	{
-		x[i]*=alpha;
-	}
+
 }
 
 void gemv(REAL alpha, REAL** A, REAL* x, REAL beta, REAL* y, int rows, int cols)
 {
-	REAL sum;
 
-	for (int i=0;i<rows;i++)
-	{
-		sum=0;
-		for (int j=0;j<cols;j++)
-		{
-			sum+=A[i][j]*x[j];
-		}
-
-		y[i]=alpha*sum + beta*y[i];
-	}
 }
 
 void scal2Dfield(REAL alpha, REAL** X, int sizeX, int sizeY)
 {
-	for (int i=0;i<sizeX;i++)
-		for (int j=0;j<sizeY;j++)
-			X[i][j]*=alpha;
+
 }
 
 void axpy2Dfield(REAL alpha, REAL** X, REAL** Y, int sizeX, int sizeY)
 {
-	for (int i=0;i<sizeX;i++)
-		for (int j=0;j<sizeY;j++)
-			Y[i][j]=alpha*X[i][j]+Y[i][j];
+
 }
+*/
--- src/blatt1.c
+++ src/blatt1.c
@@ -9,43 +9,5 @@
 
 int main(int argc, char* argv[])
 {
-	/*------------------------------------------------------------------------*/
-
-
-	/*------------------------------------------------------------------------*/
-
-	/*------------------------------------------------------------------------*/
-//	REAL** field = create2Dfield(60,40);
-
-//	for (int i=0;i<60;i++)
-//	{
-//		for (int j=0;j<40;j++)
-//		{
-//			field[i][j]=M_PI * 1.0/60.0 * (REAL)i;
-//		}
-//	}
-
-//	writeVTKfileFor2DscalarField("gradientField.vtk", "gradientfield", field, 60, 40, 0.1, 0.1);
-
-//	applyFunctionTo2Dfield(sin, field, 60, 40);
-
-//	writeVTKfileFor2DscalarField("sinusField.vtk", "sinusfield", field, 60, 40, 0.1, 0.1);
-
-//	destroy2Dfield(field, 60);
-//	/*------------------------------------------------------------------------*/
-
-
-//	/*------------------------------------------------------------------------*/
-//	int sizeX, sizeY;
-
-//	REAL** fieldU = read2Dfield("fieldU.dat", &sizeX, &sizeY);
-//	REAL** fieldV = read2Dfield("fieldV.dat", &sizeX, &sizeY);
-
-//	writeVTKfileFor2DvectorField("vectorField.vtk", "vectorfield", fieldU, fieldV, sizeX, sizeY, 0.1, 0.1);
-
-//	destroy2Dfield(fieldU, sizeX);
-//	destroy2Dfield(fieldV, sizeX);
-//	/*------------------------------------------------------------------------*/
-
 	return 0;
 }
